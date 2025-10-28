00c1daafd0ffbc9334be46d8cfb7c25f63317d3d
Ulrich Lindberg <ulrich.olsen@gmail.com>
2025-10-28 16:03:33 +0100

    Updates

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # TAC Extract
+Extract Time Activity Curves from Dynamic PET DICOM data.
+
+> Only works for PET/CT/MR data acquired on the same scanner as reslicing is done based on scanner coordinates.
 
 ## Installation
 To install this library
@@ -7,9 +10,9 @@ pip install git+https://github.com/ulindberg/tacextract.git
 ```
 
 ## Usage
-```cmd
+```python
 from tacextract import main
 from pathlib import Path
 
-main(Path(".data/dicom_series_folder"), Path(".data/TotalSegmentator.nii.gz"), Path(".data/"))
+main(Path(".data/dicom_series_folder"), Path(".data/TotalSegmentator.nii.gz"), Path(".data/"), region="brain")
 ```
--- src/tacextract/tacs.py
+++ src/tacextract/tacs.py
@@ -2,17 +2,18 @@
 from pathlib import Path
 import numpy as np
 import csv
+import re
 
 def tacwrite(FrameTimesStart:np.ndarray,FrameDuration:np.ndarray, tac:np.ndarray, tacfile:Path, unit:str='Bq/ml', label=None):
     """ Write time activity curve as PMOD (.tac) file format
     
     Args:
-            FrameTimesStart:
-            FrameDuration:
-            tac:
-            tacfile:
-            unit:
-            label:
+        FrameTimesStart:
+        FrameDuration:
+        tac:
+        tacfile:
+        unit:
+        label:
 
         Returns:
             tacfile
@@ -40,3 +41,32 @@ def tacwrite(FrameTimesStart:np.ndarray,FrameDuration:np.ndarray, tac:np.ndarray
         writer.writerows(outarray)
     
     return tacfile
+
+def tacread(filename: str):
+	""" Read Time Activity Curve (tac) file
+
+	Parameters
+	----------
+	filename : str
+	   filename of tacfile
+
+	"""
+
+	# Read header row
+	f = open(filename)
+	header = f.readline().split('\t')
+
+	# Read data
+	tac = np.loadtxt(filename, delimiter='\t', skiprows=1, comments='#')
+	
+	if tac.ndim == 1:
+		tac= tac[None,:]
+	units = {}
+
+	# Get timeunit from first column header
+	units[0] = re.search(r"(?<=\[)[^)]*(?=\])",header[0]).group(0)
+
+	# Get concentration unit from second column header
+	units[1] = re.search(r"(?<=\[)[^)]*(?=\])",header[1]).group(0)
+
+	return tac, header, units
